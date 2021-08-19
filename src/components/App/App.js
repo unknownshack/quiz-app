19c50a4bdc9d394b0eff71967dd6db6a1f8e9477
Rahul Kawari <kawarirahul@gmail.com>
2021-08-19 19:07:08 +1000

    Add react-router-dom
    Add routes for different components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,28 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Navbar from '../Navbar/Navbar';
 import Home from '../Home/Home';
+import Quiz from '../Quiz/Quiz';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div>
+          <Switch>
+            <Route path='/quiz'>
+              <Quiz />
+            </Route>
+
+            <Route exact path='/'>
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
