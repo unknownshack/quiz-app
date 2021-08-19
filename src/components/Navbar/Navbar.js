73046609d13963e05af581f5d375241b2a3ebd03
Rahul Kawari <kawarirahul@gmail.com>
2021-08-19 19:05:39 +1000

    Add Navbar component
    Convert into ES6 class component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,66 +1,90 @@
 // Use React Hooks' useState
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './Navbar.css';
 
-const Navbar = () => {
-    const [isOpen, setOpen] = useState(false);
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container-fluid">
-                <NavLink
-                    className='navbar-brand'
-                    activeClassName='is-active'
-                    aria-current="page"
-                    to='/'
-                    exact
-                >
-                    AiQ
-                </NavLink>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#menu-links"
-                    aria-controls="menu-links"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                    onClick={() => setOpen(!isOpen)}
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="menu-links">
-                    <div className="navbar-nav active">
-                        <NavLink
-                            className='nav-link'
-                            activeClassName='is-active'
-                            aria-current="page"
-                            to='/'
-                            exact
-                        >
-                            Home
-                        </NavLink>
-                        <NavLink
-                            className='nav-link'
-                            activeClassName='is-active'
-                            aria-current="page"
-                            to='/Quiz'
-                        >
-                            Demo
-                        </NavLink>
-                        {/* <NavLink
-                            className='nav-link'
-                            activeClassName='is-active'
-                            aria-current="page"
-                            to='/Contact'
-                        >
-                            Contact
-                        </NavLink> */}
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false
+        }
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
+
+    handleButtonClick() {
+        if (this.state.isOpen) {
+            this.setState({
+                isOpen: false
+            })
+        } else {
+            this.setState({
+                isOpen: true
+            })
+        }
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <NavLink
+                        className='navbar-brand'
+                        activeClassName='is-active'
+                        aria-current="page"
+                        to='/'
+                        exact
+                    >
+                        Mettle
+                    </NavLink>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#menu-links"
+                        aria-controls="menu-links"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={this.handleButtonClick}
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="menu-links">
+                        <div className="navbar-nav active">
+                            <NavLink
+                                className='nav-link'
+                                activeClassName='is-active'
+                                aria-current="page"
+                                to='/'
+                                exact
+                            >
+                                Home
+                            </NavLink>
+                            <NavLink
+                                className='nav-link'
+                                activeClassName='is-active'
+                                aria-current="page"
+                                to='/quiz'
+                            >
+                                Demo
+                            </NavLink>
+                            <NavLink
+                                className='nav-link'
+                                activeClassName='is-active'
+                                aria-current="page"
+                                to='/register'
+                                data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop"
+                            >
+                                Register your interest
+                            </NavLink>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>    )
+            </nav>
+        )
+    }
 }
 
 export default Navbar;
