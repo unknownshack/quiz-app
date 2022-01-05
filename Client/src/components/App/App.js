cc8c4f90481721426aae1f84831cdc60407a108e
Rahul Kawari <kawarirahul@gmail.com>
2022-01-05 20:21:05 +1100

    fix quiz route

--- Client/src/components/App/App.js
+++ Client/src/components/App/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
-
+import Quiz from '../Quiz/Quiz';
 
 export default function App() {
   return (
@@ -15,6 +15,7 @@ export default function App() {
           <Route path="/" element={<Navbar />}>
             <Route index element={<Home />} />
             <Route path = "login" element={<Login />} />
+            <Route path = "quiz" element={<Quiz />} />
           </Route>
         </Routes>
 
